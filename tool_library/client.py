19aa0d29944d1a8ec2ec878ccb5e8567c51dc6cf
Adam Tomkins <artomkins@googlemail.com>
2024-01-23 18:48:16 +0000

    update client

--- tool_library/client.py
+++ tool_library/client.py
@@ -6,22 +6,27 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.getLevelName(os.environ.get("TOOL_LIBRARY_LOG_LEVEL", "INFO")))
 
 class ToolLibraryClient:
-    def __init__(self, service_url: str, api_key: str):
+    def __init__(self, service_url: str, api_key: str=None):
+
+        logger.debug(f"Initializing Tool Library for service url {service_url}")
+        logger.debug(f"Using API Key: {api_key is None}")
         self.service_url = service_url
         self.api_key = api_key
         self.headers = {"X-API-KEY": api_key}
 
-        # Check health on initialization            
-        assert self.health().status_code == 200
+        # Check health on initialization          
+          
+        assert self.health()["status_code"] == 200
 
     def register_api_tool(self, tool_url, tool_routes=None, tool_api_key=None):
+
+
         if tool_api_key is not None:
             logger.warning("Tool APIS not yet supported")
 
         # TODO: Add API KEY to Headers
 
         data = {"service_url": tool_url, "tool_routes": tool_routes}
-        
         response = requests.post(f"{self.service_url}/register-api-tool/", headers=self.headers, json=data)
         response.raise_for_status()
         return response.json()
@@ -60,6 +65,7 @@ class ToolLibraryClient:
         return response.json()
 
     def health(self):
+        logger.debug(f"Checking Health on {self.service_url}/health/ from the Client")
         response = requests.get(f"{self.service_url}/health/", headers=self.headers)
         response.raise_for_status()
         return response.json()

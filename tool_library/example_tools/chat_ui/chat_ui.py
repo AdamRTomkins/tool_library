e2cb34239c01b94c5be7c79fe141d6fb4d52f3e7
Adam Tomkins <a.tomkins@digital-science.com>
2024-03-13 11:45:48 +0000

    add file hashing and serving

--- tool_library/example_tools/chat_ui/chat_ui.py
+++ tool_library/example_tools/chat_ui/chat_ui.py
@@ -44,51 +44,24 @@ import requests
 from typing import List, Dict, Any
 
 # Move these too a specific REPO for the RAG when we have it
-class FAISSClient:
-    def __init__(self, base_url: str, api_key: str):
-        self.base_url = base_url
-        self.api_key = api_key
-        self.headers = {"X-API-KEY": self.api_key}
-
-    def search(self, query: str) -> List[Dict[str, Any]]:
-        response = requests.get(f"{self.base_url}/search/", params={"query": query}, headers=self.headers)
-        response.raise_for_status()
-        return response.json()
-
-    def add_items(self, items: List[Dict[str, Any]]) -> Dict[str, Any]:
-        response = requests.post(f"{self.base_url}/add/", json=items, headers=self.headers)
-        response.raise_for_status()
-        return response.json()
-
-    def clear_index(self) -> Dict[str, Any]:
-        response = requests.post(f"{self.base_url}/clear/", headers=self.headers)
-        response.raise_for_status()
-        return response.json()
-
-    def delete_items(self, criteria: Dict[str, Any]) -> Dict[str, Any]:
-        response = requests.post(f"{self.base_url}/delete/", json=criteria, headers=self.headers)
-        response.raise_for_status()
-        return response.json()
-
-    def update_items(self, updates: Dict[str, Any]) -> Dict[str, Any]:
-        response = requests.post(f"{self.base_url}/update/", json=updates, headers=self.headers)
-        response.raise_for_status()
-        return response.json()
-
-    def index_info(self) -> Dict[str, Any]:
-        response = requests.get(f"{self.base_url}/info/", headers=self.headers)
-        response.raise_for_status()
-        return response.json()
-
-# Move these too a specific REPO for the RAG when we have it
 class KnowledgeClient:
     def __init__(self, base_url: str, api_key: str):
         self.base_url = base_url
         self.api_key = api_key
-        self.headers = {"X-API-KEY": self.api_key}
+        self.headers = {
+            "X-API-KEY": self.api_key,
+            'accept': 'application/json'
+        }
 
     def search(self, query: str, namespace:str) -> List[Dict[str, Any]]:
-        response = requests.get(f"{self.base_url}/search/", params={"query": query,"namespace":namespace}, headers=self.headers)
+        response = requests.get(f"{self.base_url}/search/", 
+            params={
+                "query": query,
+                "index_name":namespace,
+                "top_k":5,
+                "similarity_threshold":0.1
+                },
+            headers=self.headers)
         response.raise_for_status()
         return response.json()
 
@@ -117,45 +90,6 @@ class KnowledgeClient:
         response.raise_for_status()
         return response.json()
 
-class KBRetriever(BaseRetriever):
-    kb_client: Optional[KnowledgeClient] = None
-
-    def __init__(self, base_url: str, api_key: str):
-        super().__init__()
-        self.kb_client = KnowledgeClient(base_url, api_key)
-
-    def _get_relevant_documents(
-        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
-    ) -> List[Document]:
-        try:
-            search_results = self.kb_client.search(query)
-            documents = [Document(page_content=result['text'], metadata=result['attributes']) for result in search_results]
-        except Exception as e:
-            documents = []
-            logger.error(f"Error retrieving documents: {e}")
-        return documents
-
-
-class KalavaiRetriever(BaseRetriever):
-    faiss_client: Optional[FAISSClient] = None
-
-    def __init__(self, base_url: str, api_key: str):
-        super().__init__()
-        self.faiss_client = FAISSClient(base_url, api_key)
-
-    def _get_relevant_documents(
-        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
-    ) -> List[Document]:
-        try:
-            search_results = self.faiss_client.search(query)
-            documents = [Document(page_content=result['text'], metadata=result['attributes']) for result in search_results]
-        except Exception as e:
-            documents = []
-            logger.error(f"Error retrieving documents: {e}")
-        return documents
-
-
-
 
 @cl.password_auth_callback
 def auth_callback(username: str, password: str):
@@ -174,6 +108,7 @@ def auth_callback(username: str, password: str):
         )
     else:
         return None
+
 
 @cl.on_chat_start
 async def on_chat_start():  
@@ -195,103 +130,24 @@ async def on_chat_start():
         llm = ChatOpenAI(model=LLM_MODEL)
 
     RETRIEVER_BASE_URL = cl.user_session.get("user").metadata.get("knowledge_base_url")
+    
     RETRIEVER_API_KEY = cl.user_session.get("user").metadata.get("api_key")
 
-    RETRIEVER_BASE_URL = "http://127.0.0.1:8011"
+    RETRIEVER_BASE_URL = "https://knowledgebase.test.k8s.mvp.kalavai.net"
     RETRIEVER_API_KEY = None
 
-    retriever = KBRetriever(base_url=RETRIEVER_BASE_URL, api_key=RETRIEVER_API_KEY)
-
-    cl.user_session.set("retriever", retriever)
-
-    def format_docs(docs):
-        return "\n\n".join([d.page_content for d in docs])
-
-    chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
+    client = KnowledgeClient(
+        base_url=RETRIEVER_BASE_URL, 
+        api_key= RETRIEVER_API_KEY
     )
 
-    cl.user_session.set("runnable", chain)
-
+    cl.user_session.set("client", client)
+    cl.user_session.set("prompt", prompt)
+    cl.user_session.set("namespace", "carlosfm")
+    cl.user_session.set("llm", llm)
+    
     msg = await cl.Message(content="Welcome", actions = forever_actions()).send()
 
-async def knowledge_describe(**args):
-
-    # get the faiss client
-    retriever = cl.user_session.get("retriever")
-    faiss_client = retriever.faiss_client
-    url = faiss_client.base_url
-    api_key = faiss_client.api_key
-
-    info = faiss_client.index_info()
-    num = info["number_of_items"]
-    
-    await cl.Message(content=f"Your Knowledge Base: \n- URL: {url} \n- API_KEY {api_key}\n Number of Documents: {num}").send()
-
-
-async def knowledge_update(url=None, api_key=None):
-
-    # get the faiss client
-    retriever = cl.user_session.get("retriever")
-    faiss_client = retriever.faiss_client
-    updates = []
-    if url:
-        faiss_client.base_url = url
-        updates.append("URL")
-    if api_key:
-        faiss_client.api_key = api_key
-        updates.append("API KEY")
-    url = faiss_client.base_url
-    api_key = faiss_client.api_key
-
-    await cl.Message(content=f"Updating the Knowledge Base: \n- URL: {url} \n- API_KEY {api_key}").send()
-
-async def knowledge_search(text:str):
-    # get the faiss client
-    retriever = cl.user_session.get("retriever")
-    faiss_client = retriever.faiss_client
-    docs = faiss_client.search(text)
-
-    for doc in docs:
-        await cl.Message(content=doc).send()
-
-cli = {
-    "knowledge": {
-        "description": "This tool allows you to search for knowledge",
-        "functions": {
-            "describe": knowledge_describe,
-            "update": knowledge_update,
-        }
-    }
-}
-
-async def triage(message: cl.Message):
-    # if its about 
-    if message.content.startswith("/"):
-        args = message.content[1:].split(" ")
-        command = args[0]
-        
-        if len(args) < 2:
-            function = "describe"
-            kwargs = {}
-        else:
-            function = args[1]
-            kwargs = args[2:]
-            kwargs = {k.lower(): v for k, v in [kw.split("=") for kw in kwargs]}
-
-        if command in cli:
-            if function in cli[command]["functions"]:
-                await cli[command]["functions"][function](**kwargs)
-            else:
-                await cl.Message(content=f"I don't understand that function {function}. Please use one of {cli[command]['functions'].keys()}").send()
-        else:
-            await cl.Message(content=f"I don't understand that command {command}. Please use one of {cli.keys()}").send()
-        return True
-    
-    return False
 
 
 @cl.action_callback("index_files")
@@ -347,19 +203,59 @@ def forever_actions():
 
 @cl.on_message
 async def on_message(message: cl.Message):
+        
+    # Pull the session vars
+    client = cl.user_session.get("client")
+    prompt = cl.user_session.get("prompt")
+    namespace = cl.user_session.get("namespace")
+    llm = cl.user_session.get("llm")
 
-    triaged = await triage(message)
-    if triaged: return
-    
-    runnable = cl.user_session.get("runnable")  # type: Runnable
     msg = cl.Message(content="")
+    await msg.send()
 
-    async for chunk in runnable.astream(
-        {"question": message.content},
-        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
-    ):
-        await msg.stream_token(chunk)
+    # Search The Rag Instance
+    with cl.Step(name="Search") as step:
+        step.input = message.content
+
+        try:
+            context = client.search(message.content, namespace=namespace)
+        except Exception as e:
+            step.output = str(e)
+            msg.content = str(e)
+            await msg.update()
+            return
+        elements = [
+            cl.Text(name="Result", content=str(p["page_content"]), display="inline") for p in context
+        ]
+
+        element_names = "Relavent Documents: \n " + "\n".join([f'{p["metadata"]["source"]} page {p["metadata"]["page"]}' for p in context])
+
+        step.elements = elements
+        step.output = element_names
+
+        if len(context) == 0:
+            # This checks if we have found any context in the retrieval
+            msg.content = "No context found"
+            await msg.update()
+            return
+        
+        context_str  = "\n".join([p["page_content"] for p in context])
+
+        # Limit the context in tesgint:
+        context_str = context_str[:1000]
+
+        await msg.update()
+
+    messages = prompt.format_messages(context=context_str, question=message.content)
+
+    with cl.Step(name="Generate") as step:
+        step.input = message.content
+        reply = await llm.ainvoke(messages)
+        step.output = reply.content
+        msg.content = reply.content
+        await msg.update()
+        
 
     msg.actions = forever_actions()
-    msg.update()
+    await msg.update()
     await msg.send()
--- tool_library/example_tools/chat_ui/dev-launch.sh
+++ tool_library/example_tools/chat_ui/dev-launch.sh
@@ -3,18 +3,19 @@
 #export LLM_API_KEY="sk-..."
 #export LLM_MODEL="gpt-3.5-turbo"
 
-export LLM_API_KEY="None"
-export LLM_MODEL="phi-2"
-export LLM_BASE_URL="http://178.62.13.8:31095"
+export LLM_API_KEY="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"
+export LLM_MODEL="gpt-3.5-turbo-0125"
+export ANVIL_UPLINK_KEY="client_A2ZZVMV6LLHVCZ4XSMTOWTSH-ADMVMYW5OIRPH75P"
+#export LLM_BASE_URL="http://178.62.13.8:31095"
 
 
-export RETRIEVER_BASE_URL="http://178.62.13.8:31645"
-export RETRIEVER_API_KEY="None"
+#export RETRIEVER_BASE_URL="http://178.62.13.8:31645"
+#export RETRIEVER_API_KEY="None"
 
-export CHAT_USE_AUTH="True"
-export USERNAME="Adam"
-export PASSWORD="1234"
+#export CHAT_USE_AUTH="True"
+#export USERNAME="Adam"
+#export PASSWORD="1234"
 
 #pip install -r requirements.txt 
 sh prepare.sh
-chainlit run chat_ui.py -h --port 8011 -w
+chainlit run chat_ui.py -h --port 8012 -w
